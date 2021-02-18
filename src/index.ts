55eb6f86218314c850dd4492b445e333640c5fd6
Wise Introvert <fardeen.wi.panjwani@gmail.com>
2021-02-18 14:11:34 +0530

    feat(src/index.ts): class and types setup
    create interfaces for Block, Blockchain and Transactions. Create Blockchain class with
    createNewBlock and getChain methods.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,58 @@
-const a: string = "Hello";
-const b: number = 3;
+interface Transaction {}
 
-console.log(`${a} x ${b}`);
+interface Block {
+  transactions: Transaction[];
+  nonce: number;
+  hash: string;
+  previousBlockHash: string;
+  timestamp: Date | number;
+  index: number;
+}
+
+interface IBlockchain {
+  createNewBlock: (
+    _nonce: number,
+    _previousBlockHash: string,
+    _hash: string
+  ) => Block;
+  getChain: () => Block[];
+}
+
+class Blockchain implements IBlockchain {
+  private chain: Block[];
+  private newTransactions: Transaction[];
+
+  constructor() {
+    this.chain = [];
+    this.newTransactions = [];
+  }
+
+  createNewBlock = (
+    _nonce: number,
+    _previousBlockHash: string,
+    _hash: string
+  ): Block => {
+    const block: Block = {
+      transactions: this.newTransactions,
+      nonce: _nonce,
+      hash: _hash,
+      previousBlockHash: _previousBlockHash,
+      timestamp: Date.now(),
+      index: this.chain.length + 1,
+    };
+
+    this.chain.push(block);
+    this.newTransactions = [];
+
+    return block;
+  };
+
+  getChain = (): Block[] => {
+    return this.chain;
+  };
+}
+
+const blockchain: Blockchain = new Blockchain();
+console.log(blockchain.getChain());
+blockchain.createNewBlock(1, "abc", "def");
+console.log(blockchain.getChain());
